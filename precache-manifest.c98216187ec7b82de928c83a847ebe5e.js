d7fc46711311d37b840f396ca1228862f2d0b34b
Dariusz Okonski <darek200180@gmail.com>
2021-03-09 23:16:19 +0000

    Updates

---
+++ precache-manifest.c98216187ec7b82de928c83a847ebe5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7f1788cf8192c0d638ace4310cd729c",
+    "url": "/react-dice-game-v2/index.html"
+  },
+  {
+    "revision": "6cf2e1000c528de275e4",
+    "url": "/react-dice-game-v2/static/css/main.76cd3545.chunk.css"
+  },
+  {
+    "revision": "93c4ea36e03aa0c55cf3",
+    "url": "/react-dice-game-v2/static/js/2.15e688e6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-dice-game-v2/static/js/2.15e688e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cf2e1000c528de275e4",
+    "url": "/react-dice-game-v2/static/js/main.2b5b588e.chunk.js"
+  },
+  {
+    "revision": "cfb23cd34fc7a85267db",
+    "url": "/react-dice-game-v2/static/js/runtime-main.961d65d2.js"
+  }
+]);
